f43bc752c34806bb8ff42fa82bac26844a35936a
JeonghakHeo <jeonghak4142@gmail.com>
2024-04-15 09:10:58 +1000

    Added invalidation step to build

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
-      filename: 'remoteEntry.jss',
+      filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
